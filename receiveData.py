abfeb6df7b5c35eb08312932d61230a3f9754c1c
snoles27 <108779940+snoles27@users.noreply.github.com>
2023-11-10 12:03:59 -0800

    receiving data partially working

--- receiveData.py
+++ receiveData.py
@@ -1,3 +1,44 @@
 import serial
 
 
+SERIALPORT3 = "/dev/tty.usbmodem14301"
+BAUDRATE = 9600
+READATTEMPTTIMEOUT = 0.5
+
+LINESTART = 2
+UUIDSTART = 14
+LINEEND = -5
+MINLEN = 4
+EVENTENDMARKER = "b'COMPLETE\\r\\n'"
+
+class event:
+    def __init__(self, uuid:str, data:list[int] = [], times:list[int] = []):
+        self.uuid = uuid
+        self.data = data.copy()
+        self.times = times.copy()
+
+def readEventData(openPort):
+
+    with openPort as ser:
+        line = str(ser.readline())
+        if len(line) >= MINLEN:
+            uuid = line[UUIDSTART:LINEEND]
+            secondLine = str(ser.readline())
+            line = str(ser.readline())
+            while line != EVENTENDMARKER:
+                print(processLine(line))
+                line = str(ser.readline())
+        else:
+            print("NO EVENT")
+            return None
+
+def processLine(line) -> list[int]:
+
+    splitList = line[LINESTART:LINEEND].split(", ")
+    return [int(ele) for ele in splitList]
+    
+
+if __name__ == "__main__":
+    ser = serial.Serial(SERIALPORT3, BAUDRATE, timeout = READATTEMPTTIMEOUT)
+    readEventData(ser)
+    ser.close()
