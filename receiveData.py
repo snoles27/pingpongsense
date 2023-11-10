93be2be9631ed7d99aae045b256564f95fea3370
snoles27 <108779940+snoles27@users.noreply.github.com>
2023-11-10 12:18:36 -0800

    about to change event class

--- receiveData.py
+++ receiveData.py
@@ -7,12 +7,19 @@ READATTEMPTTIMEOUT = 0.5
 
 LINESTART = 2
 UUIDSTART = 14
-LINEEND = -5
+LINEEND = -5 
 MINLEN = 4
 EVENTENDMARKER = "b'COMPLETE\\r\\n'"
 
+DATA_A0_INDEX = 2
+TIME_A0_INDEX = 1
+DATA_A1_INDEX = 5
+TIME_A1_INDEX = 4
+DATA_A2_INDEX = 8
+TIME_A2_INDEX = 7
+
 class event:
-    def __init__(self, uuid:str, data:list[int] = [], times:list[int] = []):
+    def __init__(self, uuid:str, data:list[list[int]] = [[]], times:list[list[int]] = [[]]):
         self.uuid = uuid
         self.data = data.copy()
         self.times = times.copy()
@@ -22,12 +29,22 @@ def readEventData(openPort):
     with openPort as ser:
         line = str(ser.readline())
         if len(line) >= MINLEN:
+            dataMat = []
+            timesMat = []
             uuid = line[UUIDSTART:LINEEND]
             secondLine = str(ser.readline())
             line = str(ser.readline())
             while line != EVENTENDMARKER:
-                print(processLine(line))
+                #process the line that was read in
+                lineData = processLine(line)
+                print(lineData)
+                dataInLine = [lineData[DATA_A0_INDEX], lineData[DATA_A1_INDEX], lineData[DATA_A2_INDEX]]
+                timeInLine = [lineData[TIME_A0_INDEX], lineData[TIME_A1_INDEX], lineData[TIME_A2_INDEX]]
+                dataMat.append(dataInLine)
+                timeInLine.append(timeInLine)
                 line = str(ser.readline())
+            
+            return event(uuid, dataMat, timesMat)
         else:
             print("NO EVENT")
             return None
@@ -40,5 +57,9 @@ def processLine(line) -> list[int]:
 
 if __name__ == "__main__":
     ser = serial.Serial(SERIALPORT3, BAUDRATE, timeout = READATTEMPTTIMEOUT)
-    readEventData(ser)
+    eventData = readEventData(ser)
     ser.close()
+
+    if eventData is not None:
+        for row in eventData.data:
+            print(row)
