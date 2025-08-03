ba407b139b59963ae470e8135273f63d9d2b9b29
snoles <snoles27@icloud.com>
2025-08-03 14:37:52 -0700

    updated LiveRunning with the refactor

--- liveRunning.py
+++ liveRunning.py
@@ -1,26 +1,26 @@
 import eventAnalysis as anl
-import receiveData as rec
+import ReceiveData as rec
 import serial
 
-#Classification Constants (derived with eventAnalysis.py)
+# Classification Constants (derived with eventAnalysis.py)
 MIN_RMS_FREQ = 1163
 MAX_RMS_FREQ = 1915
 THRESH = 0.316
 
 if __name__ == "__main__":
-    folderName = "Data/RawEventData/"
-    ser = serial.Serial(rec.SERIALPORT3, rec.BAUDRATE, timeout = rec.READATTEMPTTIMEOUT)
+    folder_name = "Data/RawEventData/"
+    ser = serial.Serial(rec.SERIALPORT3, rec.BAUDRATE, timeout=rec.READ_ATTEMPT_TIMEOUT)
 
-    countMiss = 0
+    count_miss = 0
     while True:
-        eventData = rec.readEventData(ser, requestLabel=False)
-        if eventData is not None:
-            print(anl.classify_RMS(eventData, MIN_RMS_FREQ, MAX_RMS_FREQ, THRESH))
+        event_data = rec.read_event_data(ser, request_label=False)
+        if event_data is not None:
+            print(anl.classify_RMS(event_data, MIN_RMS_FREQ, MAX_RMS_FREQ, THRESH))
         else:
-            countMiss = countMiss + 1
-            if countMiss > 10:
+            count_miss = count_miss + 1
+            if count_miss > 10:
                 leave = input("Want to stop? (Y/N)")
                 if leave == "Y":
                     break
                 else:
-                    countMiss = 0
+                    count_miss = 0
